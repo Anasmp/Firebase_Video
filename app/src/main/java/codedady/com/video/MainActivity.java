17d17dd079ff910d002c294b1988ab60d4dcaf21
youngtechydotcom <youngtechy.com@gmail.com>
2018-04-04 14:26:35 +0530

    Initial commit

---
+++ app/src/main/java/codedady/com/video/MainActivity.java
@@ -0,0 +1,111 @@
+package codedady.com.video;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+    private Uri videouri;
+    private static final int REQUEST_CODE = 101;
+    private StorageReference videoref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+        videoref =storageRef.child("/videos" + "/userIntro.3gp");
+
+
+
+    }
+
+    public void upload(View view) {
+        if (videouri != null) {
+            UploadTask uploadTask = videoref.putFile(videouri);
+
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,
+                            "Upload failed: " + e.getLocalizedMessage(),
+                            Toast.LENGTH_LONG).show();
+
+                }
+            }).addOnSuccessListener(
+                    new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(MainActivity.this, "Upload complete",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }).addOnProgressListener(
+                    new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            updateProgress(taskSnapshot);
+
+                        }
+                    });
+        } else {
+            Toast.makeText(MainActivity.this, "Nothing to upload",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void updateProgress(UploadTask.TaskSnapshot taskSnapshot) {
+
+        @SuppressWarnings("VisibleForTests") long fileSize =
+                taskSnapshot.getTotalByteCount();
+
+        @SuppressWarnings("VisibleForTests")
+        long uploadBytes = taskSnapshot.getBytesTransferred();
+
+        long progress = (100 * uploadBytes) / fileSize;
+
+        ProgressBar progressBar = (ProgressBar) findViewById(R.id.pbar);
+        progressBar.setProgress((int) progress);
+    }
+
+    public void record(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode,
+                                    int resultCode, Intent data) {
+        videouri = data.getData();
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Video saved to:\n" +
+                        videouri, Toast.LENGTH_LONG).show();
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Failed to record video",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
